f1885ddba3a97da1435940de97af5554368c83e8
orama254 <orama209@gmail.com>
2020-12-02 08:34:23 +0300

    add src folder

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './reactdevske.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Contribute to creating The Kenya React Developer Community Here 
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/reactdeveloperske/reactdevske-website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contribute on Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
