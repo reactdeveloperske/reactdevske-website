4152c83b54032714ab5c3a1ae4065c84f5235f55
chémi <cw.nuramirah@gmail.com>
2022-06-13 18:57:21 +0800

    Add responsiveness
    When the screen (browser window) gets smaller than 768px, navbar menu will be toggled by hamburger button

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css"
 import { Link } from "react-router-dom";
+import { FiMenu, FiX } from 'react-icons/fi';
 import logo from '../../reactdevske.svg';
 
 const NavBar = () => {
+    const [active, setActive] = useState(false);
+
     return (
         <div data-testid="NavBar" className="navbar">
             <Link to="/">
                 <img src={logo} className="navbar-logo" alt="reactdevske logo" />
             </Link>
-            <ul>
-                <li><Link to="/about">About Us</Link></li>
-                <li><Link to="/members">Members</Link></li>
-                <li><Link to="/events">Events</Link></li>
-                <li><Link to="/news">News</Link></li>
-                <li><Link to="/forum">Forum</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                {/* <li><Link to="/#"><button>Join Community</button></Link></li> */}
-            </ul>
-            <Link to="/#"><button>Join Community</button></Link>
+            <div className="navbar-responsive">
+                <div className="navbar-toggle-btn" onClick={() => setActive(!active)}>
+                    {active ? <FiX /> : <FiMenu />}
+                </div>
+                <div className={`navbar-menu ${active ? 'active' : ''}`}>
+                    <ul>
+                        <li><Link to="/about">About Us</Link></li>
+                        <li><Link to="/members">Members</Link></li>
+                        <li><Link to="/events">Events</Link></li>
+                        <li><Link to="/news">News</Link></li>
+                        <li><Link to="/forum">Forum</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                    <Link to="/#"><button>Join Community</button></Link>
+                </div>
+            </div>
         </div>
     )
 }
