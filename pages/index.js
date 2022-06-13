3226d80912ad1eed06108046c424f1e1570c3017
orama254 <orama209@gmail.com>
2022-06-13 19:45:39 +0300

    feat(seo): add metadata information to site

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,14 @@ export default function Home() {
 
 		{/* <!-- Open Graph / Facebook --> */}
 		<meta property="og:type" content="website" key="og-type"/>
-		<meta property="og:url" content="https://reactdevske.netlify.app/" key="og-url"/>
+		<meta property="og:url" content="https://reactdevske.vercel.app/" key="og-url"/>
 		<meta property="og:title" content="Reactjs Developer Community in Kenya" key="og-title"/>
 		<meta property="og:description" content="React js Developer Community Kenya is a community of react js developers using react related technologies in the Kenyan software development eco system." key="og-desc"/>
 		<meta property="og:image" content="https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1623830704/og_Image_banner_reactdevske_g3htri.png" key="og-img"/>
 
 		{/* <!-- Twitter --> */}
 		<meta property="twitter:card" content="summary_large_image" key="ogtw-card"/>
-		<meta property="twitter:url" content="https://reactdevske.netlify.app/" key="ogtw-url"/>
+		<meta property="twitter:url" content="https://reactdevske.vercel.app/" key="ogtw-url"/>
 		<meta property="twitter:title" content="Reactjs Developer Community in Kenya" key="ogtw-title"/>
 		<meta property="twitter:description" content="React js Developer Community Kenya is a community of react js developers using react related technologies in the Kenyan software development eco system." key="ogtw-desc"/>
 		<meta property="twitter:image" content="https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1623830704/og_Image_banner_reactdevske_g3htri.png" key="ogtw-img" />
