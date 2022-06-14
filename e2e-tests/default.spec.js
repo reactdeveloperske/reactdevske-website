917fdb2b46b08e39072eb3143c51f6544aa984a6
Nyandika Donald <donald.nyandika@gmail.com>
2022-06-14 14:26:10 +0100

    Use playwright for e2e tests

---
+++ e2e-tests/default.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000');
+  });
+
+test.describe('Basic test', () => {
+    test('should show the title', async ({ page }) => {
+        const title = page.locator('title');
+        await expect(title).toHaveText('React Devs Kenya');
+      });
+});
