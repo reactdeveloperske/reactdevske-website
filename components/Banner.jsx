de8d9d4cfc8d6f5ba8131928db772db98b960647
bilal <b.umrani75@gmail.com>
2022-06-18 02:14:42 +0500

    [add] banner

---
+++ components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../styles/Banner.module.css";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className={styles.module}>
+        <h2 className={styles.stripe}></h2>
+        <h1 className={styles.text}>UNDER CONSTRUCTION</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Image from "next/image";
 // import styles from '../styles/Home.module.css'
 import logo from "../public/reactdevske.svg";
@@ -6,7 +7,9 @@ import logo from "../public/reactdevske.svg";
 export default function Home() {
   return (
     <div className="">
+      
       <Head>
+      <Banner />
         <title>React Devs Kenya</title>
         <meta
           name="description"
