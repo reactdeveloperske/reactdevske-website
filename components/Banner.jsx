6dfae737880160f6328f8c2ac9eca43e10174382
bilal <b.umrani75@gmail.com>
2022-06-18 16:41:03 +0500

    [add] Banner component improved [Fixes #51]

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import styles from "../styles/Banner.module.css";
 
 const Banner = () => {
   return (
     <div>
-      <div className={styles.module}>
-        <h2 className={styles.stripe}></h2>
-        <h1 className={styles.text}>UNDER CONSTRUCTION</h1>
+      <div className="banner">
+        <div className="stripe"></div>
+        <div className="text">UNDER CONSTRUCTION</div>
       </div>
     </div>
   );
 };
-
 export default Banner;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar.jsx";
 import "../styles/globals.css";
+import Banner from "../components/Banner.jsx";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Banner />
       <Navbar />
       <Component {...pageProps} />
     </>
