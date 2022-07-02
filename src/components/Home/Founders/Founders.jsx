ef69a8a38d21e25f0daced917feec7a8d708361f
zidanegimiga <zidanegimiga@gmail.com>
2022-07-02 09:57:02 +0300

    feat: create "founders" section - add tab switching logic

--- src/components/Home/Founders/Founders.jsx
+++ src/components/Home/Founders/Founders.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
-import FoundersCard from "../FoundersCard";
-import founder1 from "../../../../public/founder1.png";
-import founder2 from "../../../../public/founder2.png";
-import founder3 from "../../../../public/founder3.png";
+import React, {useState} from 'react';
+import { FoundersTab, CommunityTeamTab } from './FoundersTab';
 
 const Founders = () => {
+  const [ToggleState, setToggleState] = useState(1);
+  
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+  
+  const getActiveClass = (index, className) =>
+    ToggleState === index ? className : "";
+
   return (
     <div className='mt-[64px]'>
         <h5 className='font-montserrat text-h5 text-text-gray text-center'>THE PEOPLE BEHIND</h5>
         <h2 className='font-montserrat text-h2 font-black text-center mt-[25px] mb-[60px]'>Founders</h2>
 
-        <div className='bg-green'>
-            <div className='flex align-center justify-evenly p-[80px]'>
-              <FoundersCard img={founder1}/>
-              <FoundersCard img={founder2}/>
-              <FoundersCard img={founder3}/>
+        <div className="flex items-center justify-center">
+          <div className={`rounded-t-lg py-[27px] px-[59px] flex items-center cursor-pointer ${getActiveClass(1, "bg-green")}`} 
+            onClick={() => toggleTab(1)}>
+            <span className='font-montserrat text-center'>Founders</span>
+          </div>
+
+          <div className={`rounded-t-lg py-[27px] px-[59px] flex items-center cursor-pointer ${getActiveClass(2, "bg-green")}`}
+            onClick={() => toggleTab(2)}>
+            <span className='font-montserrat text-center'>Community Team</span>
+          </div> 
+        </div>
+
+        <div className='bg-green'>      
+            <div className={`${ToggleState === 1 ? "visible" : "hidden"} ${getActiveClass(1, "")}`}>
+              <FoundersTab/>
+            </div>
+            <div className={`${ToggleState === 2 ? "visible" : "hidden"} ${getActiveClass(2, "")}`}>
+              <CommunityTeamTab/>
             </div>
         </div>
     </div>
