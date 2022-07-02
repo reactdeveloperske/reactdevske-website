1ccde2bd281eb26cb7d0ab69646ba3b0eae87be6
zidanegimiga <zidanegimiga@gmail.com>
2022-07-02 13:33:08 +0300

    feat: add events section to the home page

--- src/pages/index.page.jsx
+++ src/pages/index.page.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Suspense } from "react";
 import Banner from "../components/Banner";
 import Hero from "../components/Home/Hero";
 import About from "../components/Home/About";
@@ -9,10 +10,12 @@ import Events from "../components/Home/Events";
 import Forum from "../components/Home/Forum";
 import Contact from "../components/Home/Contact";
 import Footer from "../components/Home/Footer";
+
 import Image from "next/image";
 import logo from "../../public/reactdevske.svg";
 
-export default function Home() {
+export default function Home({mockData}) {
+  
   return (
     <div className="">
       <Head>
@@ -34,12 +37,48 @@ export default function Home() {
         <About />
         <Mission />
         <Founders />
-        <Members />        
-        <Events />
+        <Members />
+        <Suspense>
+          <Events eventsData={mockData}/>
+        </Suspense>        
         <Forum />
         <Contact />        
         <Footer />        
       </main>
     </div>
   );
+}
+
+export const getStaticProps = async () => {
+  const mockData = [
+    {title:"Weekly Standup", src:"https://bit.ly/placeholder-img", venue:"iHub", date:"2022-06-15"},
+    {title:"2nd Bi-Annual Party", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"Junior Devs Mentorship", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Devs Networking", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Hackathon", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"Ask Me Anything", src:"https://bit.ly/placeholder-img",description:"React js Developer Community Kenya is a community of react js developers using react related technologies in the Kenyan software development eco system.",venue:"iHub",date:"2022-06-15"},
+    {title:"Weekly Standup", src:"https://bit.ly/placeholder-img", venue:"iHub", date:"2022-06-15"},
+    {title:"2nd Bi-Annual Party", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"Junior Devs Mentorship", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Devs Networking", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Hackathon", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"Ask Me Anything", src:"https://bit.ly/placeholder-img",description:"React js Developer Community Kenya is a community of react js developers using react related technologies in the Kenyan software development eco system.",venue:"iHub",date:"2022-06-15"},
+    {title:"Junior Devs Mentorship", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Devs Networking", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Hackathon", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"Ask Me Anything", src:"https://bit.ly/placeholder-img",description:"React js Developer Community Kenya is a community of react js developers using react related technologies in the Kenyan software development eco system.",venue:"iHub",date:"2022-06-15"},
+    {title:"Junior Devs Mentorship", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Devs Networking", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Hackathon", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"Ask Me Anything", src:"https://bit.ly/placeholder-img",description:"React js Developer Community Kenya is a community of react js developers using react related technologies in the Kenyan software development eco system.",venue:"iHub",date:"2022-06-15"},
+    {title:"Junior Devs Mentorship", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Devs Networking", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"React Hackathon", src:"https://bit.ly/placeholder-img", description:"This is a description for event 1",venue:"iHub",date:"2022-06-15"},
+    {title:"Ask Me Anything", src:"https://bit.ly/placeholder-img",description:"React js Developer Community Kenya is a community of react js developers using react related technologies in the Kenyan software development eco system.",venue:"iHub",date:"2022-06-15"},
+  ];
+  return{
+      props:{
+          mockData
+      }
+  }
 }
