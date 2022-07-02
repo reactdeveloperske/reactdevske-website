b12868a014151acfb517cd6320044bdd6493b918
zidanegimiga <zidanegimiga@gmail.com>
2022-07-02 13:55:44 +0300

    feat: create privacy policy page

---
+++ src/pages/privacyPolicy/index.page.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <>
+      <Head>
+        <title>Reactjs Developer Community in Kenya - Privacy Policy</title>
+      </Head>
+      <main className="flex justify-center items-center min-h-screen">
+        <h1 className="text-white">Privacy Policy page will be here</h1>
+      </main>
+    </>
+  );
+}
