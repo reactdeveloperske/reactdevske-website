77d327d499fa069885ab8afdf880dbfed9f069f3
marvin espira <espiramarvin@gmail.com>
2022-10-18 20:40:35 +0300

    feat: adds events section changes

--- e2e/events.spec.ts
+++ e2e/events.spec.ts
@@ -4,22 +4,16 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000');
 });
 
-
 test.describe('Test if see more link is clickable', () => {
   test('should navigate to the correct URL when See More is clicked', async ({
     page,
   }) => {
     const [newPage] = await Promise.all([
       page.waitForEvent('popup'),
-      page.locator('#events >> text="See More"').click()
+      page.locator('#events >> text="See More"').click(),
     ]);
     await expect(newPage).toHaveURL(
       'https://kommunity.com/reactjs-developer-community-kenya-reactdevske/events'
     );
   });
-
 });
-
-
-
-
