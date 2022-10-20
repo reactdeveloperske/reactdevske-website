e086b137b77c5ffb9249870d61f7b3a074cf32f8
Antony Sande <sandeantony@gmail.com>
2022-10-20 06:58:24 +0300

    Wait for slow webkit test

--- e2e/events.spec.ts
+++ e2e/events.spec.ts
@@ -8,9 +8,12 @@ test.describe('Test if see more link is clickable', () => {
   test('should navigate to the correct URL when See More is clicked', async ({
     page,
     context,
+    browserName,
   }) => {
+    test.slow(browserName === 'webkit', 'This test times out on webkit');
     const [newPage] = await Promise.all([
       context.waitForEvent('page'),
+      page.waitForLoadState(),
       page.getByRole('link', { name: 'See More' }).click(),
     ]);
     await expect(newPage).toHaveURL(
