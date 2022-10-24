9c47798a1e3d4885ab2b3712ed9d17b16be8df46
AJ-Kulundu <jameskulundu@gmail.com>
2022-10-24 11:27:30 +0300

    Changes on bg colour, pillar min width and the html tag

--- src/components/Pillar/Pillar.tsx
+++ src/components/Pillar/Pillar.tsx
@@ -8,10 +8,10 @@ export default function Pillar({
   label: string;
 }) {
   return (
-    <div className="flex max-w-xs md:max-w-sm space-x-4 p-4 border rounded-lg items-center md:bg-[#09AFC6]">
-      <h1 className="text-2xl md:text-4xl font-extrabold md:text-white tracking-wide">
+    <div className="flex max-w-xs md:max-w-sm min-w-[50%] md:min-w-[30%]  space-x-4 p-4 border rounded-lg items-center bg-white md:bg-[#09AFC6]">
+      <p className="text-2xl md:text-4xl font-extrabold md:text-white tracking-wide">
         {count}
-      </h1>
+      </p>
       <p className=" truncate text-md md:text-xl capitalize md:text-white tracking-wide">
         {label}
       </p>
