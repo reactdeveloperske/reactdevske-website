a064bf674284f2a9ab3f024ebc4d362bdbd2d0e1
devKiratu <dev.kiratu@gmail.com>
2022-10-29 16:00:36 +0300

    add hero header tests

---
+++ e2e/hero-header.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  page.goto('http://localhost:3000');
+});
+
+test.describe('Test Hero Header Navigation Links', () => {
+  test('About us link should navigate to about us section', async ({
+    page,
+  }) => {
+    await page.locator('li:has-text("About us")').click();
+    await expect(page).toHaveURL('/#about-us');
+  });
+
+  test('Events link should navigate to Events section', async ({ page }) => {
+    await page.locator('li:has-text("Events")').click();
+    await expect(page).toHaveURL('/#events');
+  });
+
+  test('Contact link should navigate to Contact Us section', async ({
+    page,
+  }) => {
+    await page.locator('li:has-text("Contact")').click();
+    await expect(page).toHaveURL('/#contact-us');
+  });
+
+  test('Join Community link should open google form in new tab', async ({
+    page,
+    context,
+  }) => {
+    const [newPage] = await Promise.all([
+      context.waitForEvent('page'),
+      page.waitForLoadState(),
+      page.locator('text=Join Community').click(),
+    ]);
+    await expect(newPage).toHaveURL(
+      new RegExp(
+        '^https://docs.google.com/forms/d/e/1FAIpQLSc_k5sffFTeL9oDug41nXU4Spw5cV84ExaL3jNFu_I1FTZO1w/viewform'
+      )
+    );
+  });
+
+  test('Join ReactDevsKe link should open google form in new tab', async ({
+    page,
+    context,
+  }) => {
+    const [newPage] = await Promise.all([
+      context.waitForEvent('page'),
+      page.waitForLoadState(),
+      page.locator('text=Join ReactDevsKe').click(),
+    ]);
+    await expect(newPage).toHaveURL(
+      new RegExp(
+        '^https://docs.google.com/forms/d/e/1FAIpQLSc_k5sffFTeL9oDug41nXU4Spw5cV84ExaL3jNFu_I1FTZO1w/viewform'
+      )
+    );
+  });
+});
