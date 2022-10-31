3191796db3d25830c15064e91acd8598a1f49d6a
devKiratu <dev.kiratu@gmail.com>
2022-10-31 11:43:22 +0300

    replace custom dropdown menu with `headless ui` menu component

--- src/components/HeroHeader/MobileHero.tsx
+++ src/components/HeroHeader/MobileHero.tsx
@@ -1,22 +1,21 @@
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React from 'react';
 import Logo from '../Logo';
 import githubIcon from '../../../public/img/github-logo.svg';
-import menuIcon from '../../../public/img/menu-icon.svg';
-import closeIcon from '../../../public/img/close-icon.svg';
 import DropdownMenu from './DropdownMenu';
 import LinkButton from '../LinkButton/LinkButton';
+import Link from 'next/link';
+import { HOME } from '../../util/routeConstants';
 
 export default function MobileHero() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const openMenu = () => setMenuOpen(true);
-  const closeMenu = () => setMenuOpen(false);
-
   return (
     <div className="relative">
       <div className="mx-6 pt-2 pb-5 flex justify-between">
-        <Logo size={60} />
+        <Link href={HOME}>
+          <a>
+            <Logo size={60} />
+          </a>
+        </Link>
         <a
           target="_blank"
           href="https://github.com/reactdeveloperske/reactdevske-website"
@@ -25,29 +24,8 @@ export default function MobileHero() {
         >
           <Image src={githubIcon} alt="" width={35} height={35} />
         </a>
-        {menuOpen ? (
-          <div className="inline-flex justify-center items-center w-[30px] ">
-            <Image
-              src={closeIcon}
-              alt=""
-              width={20}
-              height={20}
-              className="cursor-pointer"
-              onClick={closeMenu}
-            />
-          </div>
-        ) : (
-          <Image
-            src={menuIcon}
-            alt=""
-            width={30}
-            height={30}
-            className="cursor-pointer"
-            onClick={openMenu}
-          />
-        )}
+        <DropdownMenu />
       </div>
-      {menuOpen && <DropdownMenu onCloseMenu={closeMenu} />}
       <div>
         <h1 className="font-robotoMono font-medium text-4xl text-center mb-2">
           ReactDevsKe
