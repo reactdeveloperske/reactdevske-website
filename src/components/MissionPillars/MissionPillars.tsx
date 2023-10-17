38b4523046f2384c8eb21cf7ae94edf894310e16
octaviookumu <octaviookumu@gmail.com>
2023-10-17 12:15:45 +0300

    Format to fix build error [Fixes #137]

--- src/components/MissionPillars/MissionPillars.tsx
+++ src/components/MissionPillars/MissionPillars.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Pillar from '../Pillar/Pillar';
-import {Fade} from "react-awesome-reveal"
+import { Fade } from 'react-awesome-reveal';
 
 export default function MissionPillars() {
   return (
@@ -11,65 +11,65 @@ export default function MissionPillars() {
       <div className="flex flex-col md:flex-row md:items-center md:justify-center w-full px-6 space-y-4 md:space-x-6">
         <div className="flex">
           <Fade direction="left">
-          <Pillar count="01" label="Mentorship" />
+            <Pillar count="01" label="Mentorship" />
           </Fade>
         </div>
         <div className="flex justify-end w-full md:w-2/5 lg:w-1/4">
           <div className="flex w-1/2 md:w-full">
             <div className="w-full overflow-hidden aspect-w-4 aspect-h-3">
-              <Fade direction='right'>
-              <Image
-                src={
-                  'https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1678532588/website-images/mentorship-image_tz0mor.jpg'
-                }
-                alt="Mentorship Image"
-                layout="fill"
-              />
-          </Fade>
+              <Fade direction="right">
+                <Image
+                  src={
+                    'https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1678532588/website-images/mentorship-image_tz0mor.jpg'
+                  }
+                  alt="Mentorship Image"
+                  layout="fill"
+                />
+              </Fade>
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col md:flex-row-reverse md:items-center md:justify-center w-full px-6 space-y-4 md:space-x-6 md:space-x-reverse">
         <div className="flex justify-end">
-          <Fade direction='right'>
-          <Pillar count="02" label="Learning" />
-       </Fade>
+          <Fade direction="right">
+            <Pillar count="02" label="Learning" />
+          </Fade>
         </div>
         <div className="flex md:justify-start w-full md:w-2/5 lg:w-1/4">
           <div className="flex w-1/2 md:w-full">
             <div className="w-full overflow-hidden aspect-w-4 aspect-h-3">
-              <Fade direction='left'>
-              <Image
-                src={
-                  'https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1678532588/website-images/learning-image_eylqwm.jpg'
-                }
-                alt="Learning Image"
-                layout="fill"
-              />
-             </Fade>
+              <Fade direction="left">
+                <Image
+                  src={
+                    'https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1678532588/website-images/learning-image_eylqwm.jpg'
+                  }
+                  alt="Learning Image"
+                  layout="fill"
+                />
+              </Fade>
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col md:flex-row md:items-center md:justify-center w-full px-6 space-y-4 md:space-x-6">
         <div className="flex">
-          <Fade direction='left'>
-          <Pillar count="03" label="Community" />
+          <Fade direction="left">
+            <Pillar count="03" label="Community" />
           </Fade>
         </div>
         <div className="flex justify-end w-full md:w-2/5 lg:w-1/4 ">
           <div className="flex w-1/2 md:w-full">
             <div className="w-full overflow-hidden aspect-w-4 aspect-h-3">
-              <Fade direction='right'>
-              <Image
-                src={
-                  'https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1678532588/website-images/community-image_pkeqwj.jpg'
-                }
-                alt="Community Image"
-                layout="fill"
-              />
-             </Fade>
+              <Fade direction="right">
+                <Image
+                  src={
+                    'https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1678532588/website-images/community-image_pkeqwj.jpg'
+                  }
+                  alt="Community Image"
+                  layout="fill"
+                />
+              </Fade>
             </div>
           </div>
         </div>
