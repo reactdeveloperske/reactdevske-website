2d67050165514e1725e7c5d874d0abf35d0bd757
festus-sulumeti <sulumetifestus@outlook.com>
2026-02-15 14:06:26 +0300

    chore: removed the unused image from the importation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { Menu, X } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Logo from '../Logo/Logo';
